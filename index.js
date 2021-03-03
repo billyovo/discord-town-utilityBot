b139a0b45fe6e09c5561248a2c98de9dddd69b17
billyovo <30436689+billyovo@users.noreply.github.com>
2021-03-03 10:09:57 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 const { CommandoClient } = require('discord.js-commando');
 const path = require('path');
 const config = require('./config.json');
-require('dotenv').config();
 const fetch = require("node-fetch");
 var CronJob = require('cron').CronJob;
+require('dotenv').config();
+
+const owner = config.owner;
+const prefix = config.prefix;
+const keepAwakeEndpoint = config.keepAwakeEndpoint;
 
 var keepAwake = new CronJob('*/25 * * * *', function() {
-	fetch("https://discord-bosslist-bot.herokuapp.com/",{method: "HEAD"})
+	fetch(keepAwakeEndpoint,{method: "HEAD"})
 	.then(response => {
-		console.log("bossBot's status: "+response.status);
+		console.log("bossBot's status: "+response.status+" "+response.statusText);
 	});
   }, null, true, 'Asia/Taipei');
 
 keepAwake.start();
 
 const client = new CommandoClient({
-	commandPrefix: config.prefix,
-	owner: config.owner,
+	commandPrefix: prefix,
+	owner: owner,
 });
 
 client.registry

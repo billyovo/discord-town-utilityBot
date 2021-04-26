1b118c720325548fbdb511e7204c4ce2883a20a5
billyovo <30436689+billyovo@users.noreply.github.com>
2021-04-26 16:16:26 +0800

    Update town.js

--- commands/minecraft/town.js
+++ commands/minecraft/town.js
@@ -1,1 +1,40 @@
+const { Command } = require('discord.js-commando');
+const Discord = require('discord.js');
+const fetch = require("node-fetch");
 
+module.exports = class connect extends Command{
+	constructor(client) {
+		super(client, {
+			name: 'town',
+			group: 'minecraft',
+			memberName: 'town',
+			description: 'get town info',
+		});
+	}
+
+run(message) {
+    fetch('https://towns.letsdream.today/api/v1/guilds',{method: "GET"})
+	  .then(response => {
+      return response.json();
+    })
+  .then(data => {
+      data.records.forEach((guild)=>{
+        if(guild.name === '非洲渡喧'){
+          let date = new Date(guild.creationDate);
+         const embed = new Discord.MessageEmbed()
+         .setColor('#d2691e')
+         .setURL('https://towns.letsdream.today/')
+         .setTitle('非洲渡喧')
+         .setThumbnail(message.guild.iconURL())
+         .addFields(
+		      { name: '城鎮幣', value: guild.balance },
+           { name: '創建日期', value: date.getFullYear()+'-'+date.getMonth()+'-'+date.getDate()}
+           )
+         message.channel.send(embed);
+        }
+      })
+  })
+  .catch(error => {msg.channel.send(error.message)});
+	});
+  }
+}

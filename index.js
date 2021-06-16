bda6d8f8ece87d4ab4e255f76273c411b26fc42d
billyovo <30436689+billyovo@users.noreply.github.com>
2021-06-16 21:36:09 +0800

    a

--- index.js
+++ index.js
@@ -18,12 +18,16 @@ var keepAwake = new CronJob('*/25 * * * *', function() {
   }, null, true, 'Asia/Taipei');
 
   var bday = new CronJob('*/2 * * * *', function() {
+	const locales = ["零","一","二","三","四","五","六","七","八","九"];
 	let now = DateTime.now();
 	let birthday = DateTime.fromISO("2021-01-02");
 	let diff = now.diff(birthday,'days').toObject();
 	console.log(diff);  
+	diff = diff.toString();
 	let channel = client.channels.cache.get(728613506202599474);
 	console.log(channel);
+	let middle = diff1 === 0 ? "零" : locales[diff1[1]]+"十";
+	console.log("小妹生日後第"+locales[diff[0]]+"百"+middle+locales[diff[2]]+"天後不快樂");
   }, null, true, 'Asia/Taipei');
 
 keepAwake.start();

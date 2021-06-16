a8ae699924a6e4553750646245d7b8ce1b2684c3
billyovo <30436689+billyovo@users.noreply.github.com>
2021-06-16 22:22:31 +0800

    test

--- index.js
+++ index.js
@@ -23,12 +23,11 @@ var keepAwake = new CronJob('*/25 * * * *', function() {
 	let birthday = DateTime.fromISO("2021-01-02");
 	let diff = now.diff(birthday,'days').toObject();
 	diff = diff.days.toString();
-	//let channel = client.channels.fetch(728613506202599474,true,true);
-	let channel = client.channels.cache.get(728613506202599474);
-	console.log(channel);
+	let channel = client.channels.fetch(728613506202599474,true,true);
+	//let channel = client.channels.cache.get(728613506202599474);
 	let middle = diff == 0 ? "零" : locales[diff[1]]+"十";
-	//channel.send("<@430842771847380997>, @everyone 小妹生日後第"+locales[diff[0]]+"百"+middle+locales[diff[2]]+"天後不快樂");
-	console.log("<@430842771847380997>, @everyone 小妹生日後第"+locales[diff[0]]+"百"+middle+locales[diff[2]]+"天後不快樂");
+	channel.send("<@430842771847380997>, @everyone 小妹生日後第"+locales[diff[0]]+"百"+middle+locales[diff[2]]+"天後**NEGATIVE**");
+	console.log("<@430842771847380997>, @everyone 小妹生日後第"+locales[diff[0]]+"百"+middle+locales[diff[2]]+"天後**NEGATIVE**");
   }, null, true, 'Asia/Taipei');
 
 keepAwake.start();

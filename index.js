ba54c4d7c82fd88d1b35825f51e38ce4033dad30
billyovo <30436689+billyovo@users.noreply.github.com>
2022-05-20 18:41:46 +0800

    turn over a new leaf by becoming more organized

--- index.js
+++ index.js
@@ -8,35 +8,36 @@ const fs = require("fs");
 const config = require("./config.json");
 const prefix = config.prefix;
 
-function loadCommands(){
-    bot.commands = new Discord.Collection();
-    const dirPath = path.resolve(__dirname, './discord/commands');
-    const commands = fs.readdirSync(dirPath).filter(file => file.endsWith(".js"));
-    for (const file of commands) {
-        const commandName = file.split(".")[0];
-        const command = require(dirPath+`/${file}`);
-      
-        bot.commands.set(commandName, command);
-        console.log("Loaded command: "+commandName);
-    }
+function setCommand(commandName, command){
+    bot.commands.set(commandName, command);
+    console.log("Loaded command: "+commandName);
+}
 
-    const slashDirPath = path.resolve(__dirname, './discord/slash_commands');
-    const slashCommands = fs.readdirSync(slashDirPath).filter(file => file.endsWith(".js"));
-    for (const file of slashCommands) {
-        const commandName = file.split(".")[0];
-        const command = require(slashDirPath+`/${file}`);
-      
-        bot.commands.set(commandName, command);
-        console.log("Loaded slash command: "+commandName);
+function readCommands(directory, subDirectory = ""){
+    
+    const dirPath = path.resolve(__dirname, directory);
+    const commands = fs.readdirSync(dirPath);
+
+    for(const file of commands){
+        const commandName = file.split(".");
+        if(commandName[1]){
+            const command = require(dirPath+`/${file}`);
+
+            setCommand((subDirectory ? subDirectory+"-" : subDirectory) +commandName[0], command);
+        }
+        else{
+            return readCommands(directory+"/"+commandName[0], commandName[0]);
+        }
     }
 }
 
-loadCommands();
+    bot.commands = new Discord.Collection();
+    readCommands('./discord/commands');
+    readCommands('./discord/slash_commands');
 
 bot.on('messageCreate',async (msg) => {
     if(!msg.content.startsWith(prefix)){return;}
-    if(msg.member && !msg.member.permissions.has(Permissions.FLAGS.MANAGE_WEBHOOKS)){return;}
-    let params = msg.content.substring(1).split(' ');
+    const params = msg.content.substring(1).split(' ');
     const commandName = params.shift(); 
     bot.commands.get(commandName)?.run(bot,msg,params);
 })
@@ -44,6 +45,7 @@ bot.on('messageCreate',async (msg) => {
 bot.on('interactionCreate', async (interaction) => {
     if (!interaction.isCommand()) return;
 	const { commandName } = interaction;
-    bot.commands.get(commandName)?.run(bot,interaction);   
+    const subCommand = interaction.options.getSubcommand(false);
+    bot.commands.get(subCommand ? `${commandName}-${subCommand}` : commandName)?.run(bot,interaction);   
 });
 

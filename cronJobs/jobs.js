e1e56b1019280de2064efc8aafd04a13422ae0c4
billyovo <30436689+billyovo@users.noreply.github.com>
2023-01-04 17:33:00 +0800

    refac

--- cronJobs/jobs.js
+++ cronJobs/jobs.js
@@ -2,34 +2,25 @@ const CronJob = require('cron').CronJob;
 const { bot } = require("../discord/init.js");
 const { DateTime } = require("luxon");
 
-let arrival_date = DateTime.now().setZone("Asia/Taipei").startOf("day").set({day: 24, month: 4, year: 2022});
-
 function returnDiff(now){
+	const arrival_date = DateTime.now().setZone("Asia/Taipei").startOf("day").set({day: 24, month: 4, year: 2022});
+	
     if(now.ordinal === arrival_date.ordinal){
         const diff = now.diff(arrival_date,'years').toObject();
-        return Math.round(diff.years);
+        return Math.round(diff.years)+"周年";
     }
     else{
         const diff = now.diff(arrival_date,'days').toObject();
-        return Math.round(diff.days);
+        return Math.round(diff.days)+"天";
     }
-    
+   
 }
-  const newLife = new CronJob('1 0 * * *', function() {
-	let now = DateTime.now().setZone("Asia/Taipei").endOf("day");
-	
-	let diff = returnDiff(now);
+const newLife = new CronJob('1 0 * * *', function() {
+	const now = DateTime.now().setZone("Asia/Taipei").endOf("day");
 
 	bot.channels.fetch('728613506202599474',true,false)
 	.then((channel)=> {
-		
-		if(now.ordinal === diff.ordinal){
-			channel.send("@everyone 小妹有錢人生活"+diff+"周年 **POSITIVE**");
-		}
-		else{
-			channel.send("@everyone 小妹有錢人生活第"+diff+"天 **POSITIVE**");
-		}
-		
+		channel.send("@everyone 小妹有錢人生活第"+returnDiff(now)+" **POSITIVE**");
 	} )
 	.catch((error)=>{console.error(error)})
 	

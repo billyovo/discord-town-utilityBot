99ed43d4192baf7acae4b34cfb4c12a0fe3bde81
Alex Choi <alex123321choicy@gmail.com>
2023-05-01 02:43:21 +0800

    add comments of reg script, and chinese ocr lang support

--- Register-cmd.js
+++ Register-cmd.js
@@ -2,7 +2,10 @@ const axios = require('axios');
 require('dotenv').config();
 const fs = require('node:fs');
 const path = require('node:path');
-const apiurl = `https://discord.com/api/v10/applications/${process.env.clientID}/guilds/${process.env.guildId}/commands`;
+// add AppID and guildID(Sever ID) into the .env file
+// don't ask me why use guild maybe she is in somewhere so i use guild mode and why guildID but not serverID because... i don't know
+// Copilot also don't know it suggest me to write i don't know
+const apiurl = `https://discord.com/api/v10/applications/${process.env.AppID}/guilds/${process.env.guildId}/commands`;
 const commandsPath = path.join(__dirname, 'slash_commands_json');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.json'));
 
